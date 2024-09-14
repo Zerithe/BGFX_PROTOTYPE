1c95198af6c7fd4370cc4b66d81a0bb7808ea52a
Zerithe <jonathandevera08@gmail.com>
2024-09-14 16:32:11 +0800

    Add project files.

---
+++ BGFX_PROTOTYPE.cpp
@@ -0,0 +1,133 @@
+﻿// BGFX_PROTOTYPE.cpp : Defines the entry point for the application.
+//
+
+#include <iostream>
+
+#include "BGFX_PROTOTYPE.h"
+#include <bgfx/bgfx.h>
+#include <bx/uint32_t.h>
+#include <bgfx/platform.h>
+
+#include <GLFW/glfw3.h>
+#if defined(_WIN32)
+#define GLFW_EXPOSE_NATIVE_WIN32
+#include <GLFW/glfw3native.h>
+#endif
+
+using namespace std;
+
+#define WNDW_WIDTH 1920
+#define WNDW_HEIGHT 1080
+
+struct PosColorVertex
+{
+    float x;
+    float y;
+    float z;
+    uint32_t abgr;
+};
+
+static PosColorVertex cubeVertices[] =
+{
+    {-1.0f,  1.0f,  1.0f, 0xff000000 },
+    { 1.0f,  1.0f,  1.0f, 0xff0000ff },
+    {-1.0f, -1.0f,  1.0f, 0xff00ff00 },
+    { 1.0f, -1.0f,  1.0f, 0xff00ffff },
+    {-1.0f,  1.0f, -1.0f, 0xffff0000 },
+    { 1.0f,  1.0f, -1.0f, 0xffff00ff },
+    {-1.0f, -1.0f, -1.0f, 0xffffff00 },
+    { 1.0f, -1.0f, -1.0f, 0xffffffff },
+};
+
+static const uint16_t cubeTriList[] =
+{
+    0, 1, 2,
+    1, 3, 2,
+    4, 6, 5,
+    5, 6, 7,
+    0, 2, 4,
+    4, 2, 6,
+    1, 5, 3,
+    5, 7, 3,
+    0, 4, 1,
+    4, 5, 1,
+    2, 3, 6,
+    6, 3, 7,
+};
+
+static bool s_showStats = false;
+
+static void glfw_errorCallback(int error, const char* description)
+{
+    fprintf(stderr, "GLFW error %d: %s\n", error, description);
+}
+
+static void glfw_keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_F1 && action == GLFW_RELEASE)
+        s_showStats = !s_showStats;
+}
+
+int main(void)
+{
+    glfwInit();
+    GLFWwindow* window = glfwCreateWindow(WNDW_WIDTH, WNDW_HEIGHT, "BGFX_PROTOTYPE", NULL, NULL);
+
+    glfwSetKeyCallback(window, glfw_keyCallback);
+    
+    bgfx::renderFrame();
+
+    bgfx::Init bgfxinit;
+    bgfxinit.type = bgfx::RendererType::OpenGL;
+    bgfxinit.resolution.width = WNDW_WIDTH;
+    bgfxinit.resolution.height = WNDW_HEIGHT;
+    bgfxinit.resolution.reset = BGFX_RESET_VSYNC;
+    bgfxinit.platformData.nwh = glfwGetWin32Window(window);
+    bgfx::init(bgfxinit);
+
+    bgfx::setViewRect(0, 0, 0, WNDW_WIDTH, WNDW_HEIGHT);
+    bgfx::setViewClear(0, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH, 0x07374ecff, 1.0f, 0);
+
+
+    while (!glfwWindowShouldClose(window)) {
+        // Poll events
+        glfwPollEvents();
+
+        bgfx::touch(0);
+
+        // Use debug font to print information about this example.
+        bgfx::dbgTextClear();
+        bgfx::dbgTextPrintf(0, 0, 0x0f, "Press F1 to toggle stats.");
+        bgfx::dbgTextPrintf(0, 1, 0x0f, "Color can be changed with ANSI \x1b[9;me\x1b[10;ms\x1b[11;mc\x1b[12;ma\x1b[13;mp\x1b[14;me\x1b[0m code too.");
+        bgfx::dbgTextPrintf(80, 1, 0x0f, "\x1b[;0m    \x1b[;1m    \x1b[; 2m    \x1b[; 3m    \x1b[; 4m    \x1b[; 5m    \x1b[; 6m    \x1b[; 7m    \x1b[0m");
+        bgfx::dbgTextPrintf(80, 2, 0x0f, "\x1b[;8m    \x1b[;9m    \x1b[;10m    \x1b[;11m    \x1b[;12m    \x1b[;13m    \x1b[;14m    \x1b[;15m    \x1b[0m");
+        const bgfx::Stats* stats = bgfx::getStats();
+        bgfx::dbgTextPrintf(0, 2, 0x0f, "Backbuffer %dW x %dH in pixels, debug text %dW x %dH in characters.", stats->width, stats->height, stats->textWidth, stats->textHeight);
+        // Enable stats or debug text.
+        bgfx::setDebug(s_showStats ? BGFX_DEBUG_STATS : BGFX_DEBUG_TEXT);
+
+        bgfx::frame();
+    }
+
+    bgfx::shutdown();
+    glfwTerminate();
+
+	return 0;
+}
+
+
+/*
+    glfwInit();
+    GLFWwindow* window = glfwCreateWindow(WNDW_WIDTH, WNDW_HEIGHT, "BGFX_PROTOTYPE", NULL, NULL);
+
+    bgfx::PlatformData pd;
+    pd.nwh = glfwGetWin32Window(window);
+    bgfx::setPlatformData(pd);
+
+    bgfx::Init bgfxInit;
+    bgfxInit.type = bgfx::RendererType::OpenGL; // Automatically choose a renderer.
+    bgfxInit.resolution.width = WNDW_WIDTH;
+    bgfxInit.resolution.height = WNDW_HEIGHT;
+    bgfxInit.resolution.reset = BGFX_RESET_VSYNC;
+    bgfx::init(bgfxInit);
+*/
